9fd4ae1e08635ad1baf057800db4f803cdb932e3
imunnic <144065335+imunnic@users.noreply.github.com>
2024-05-24 09:00:08 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,37 +11,18 @@ usuario=""
 direccion_ip=""
 rootPasswd=""
 
-# Configurar la IP estática
-echo "Configurando la IP estática a ${direccion_ip}"
-cat <<EOL > /etc/network/interfaces
-# The loopback network interface
-auto lo
-iface lo inet loopback
-
-# The primary network interface
-auto eth0
-allow-hotplug eth0
-iface eth0 inet static
-    address ${direccion_ip}
-    netmask 255.255.255.0
-    gateway 192.168.1.1
-EOL
-
-# Reiniciar el servicio de red para aplicar los cambios
-echo "Reiniciando el servicio de red"
-systemctl restart networking
-
 # Actualizar el sistema
 echo "Actualizando el sistema"
 apt update
 apt upgrade -y
 
+# Reinstalar OpenSSH Client y Server
+echo "Reinstalando OpenSSH Client y Server"
+apt reinstall -y openssh-client openssh-server
+
 # Instalar Docker y Docker Compose
 echo "Instalando Docker y Docker Compose"
 apt install -y docker docker-compose
-
-#Instalar sudo
-apt install sudo
 
 # Crear el usuario ${usuario} con permisos sudo
 echo "Creando el usuario ${usuario} con permisos sudo"
@@ -56,6 +37,11 @@ usermod -aG docker ${usuario}
 # Cambiar la contraseña de root
 echo "Cambiando la contraseña de root"
 echo "root:${rootPasswd}" | chpasswd
+
+# Habilitar y iniciar el servicio SSH
+echo "Habilitando y iniciando el servicio SSH"
+systemctl enable ssh
+systemctl start ssh
 
 # Crear la carpeta Portainer y el archivo docker-compose.yml
 echo "Creando la carpeta Portainer y el archivo docker-compose.yml"
@@ -85,17 +71,34 @@ echo "Creando la carpeta /home/${usuario}/plex/media para montar el dispositivo"
 mkdir -p /home/${usuario}/plex/media
 chown -R ${usuario}:${usuario} /home/${usuario}/plex/media
 
-# Agregar la entrada al archivo /etc/fstab
-echo "Agregando la entrada en /etc/fstab para montar el dispositivo con UUID 29A454401B165805"
-echo "UUID=29A454401B165805 /home/${usuario}/plex/media ntfs defaults,uid=$(id -u ${usuario}),gid=$(id -g ${usuario}) 0 0" | tee -a /etc/fstab
-
-# Montar el dispositivo
-echo "Montando el dispositivo"
-mount -a
-
 # Cambiar el directorio a /home/${usuario}/Portainer y levantar los contenedores con docker-compose
 echo "Iniciando Portainer con Docker Compose"
 cd /home/${usuario}/Portainer
 docker-compose up -d
 
-echo "Configuración completada. Por favor, reinicie el servidor."
+# Configurar la IP estática
+echo "Configurando la IP estática a ${direccion_ip}"
+cat <<EOL > /etc/network/interfaces
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto eth0
+allow-hotplug eth0
+iface eth0 inet static
+    address ${direccion_ip}
+    netmask 255.255.255.0
+    gateway 192.168.1.1
+
+# Enable DHCP for eth0 as fallback
+iface eth0 inet dhcp
+EOL
+
+# Reiniciar el servicio de red para aplicar los cambios
+echo "Reiniciando el servicio de red"
+systemctl restart networking
+
+# Reiniciar el sistema
+echo "Reiniciando el sistema"
+reboot -f
